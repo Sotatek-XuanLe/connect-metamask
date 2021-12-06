7f48e84103eafd37337555df34b3bfe19c4b6a7f
Sotatek-TamNguyen <tam.nguyen2@sotatek.com>
2021-12-06 16:03:11 +0700

    set up redux toolkit, add config babel core

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Web3 from 'web3';
+import React from "react";
+import "./App.css";
+import Web3 from "web3";
 declare global {
   interface Window {
     ethereum?: any;
@@ -9,7 +9,7 @@ declare global {
 }
 export const getSignature = (address: string): any => {
   let web3: any;
-  console.log(address, 'address');
+  console.log(address, "address");
   const _message = `Sign this message to login with address ${address}`;
   if (web3) {
     return new Promise<any>((resolve: (value: string) => void, reject) => {
@@ -23,16 +23,16 @@ export const getSignature = (address: string): any => {
   }
 };
 const App: React.FC = () => {
-  const [address, setAddress] = React.useState('');
+  const [address, setAddress] = React.useState("");
   const handleConnectMetaMask = async () => {
     if (!window.ethereum) {
-      setAddress('')
+      setAddress("");
       return;
     }
     window.web3 = new Web3(window.ethereum);
     try {
       const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
       const signature = (await getSignature(accounts[0])) as string;
       const payload = {
@@ -40,25 +40,20 @@ const App: React.FC = () => {
         password: signature,
         message: `Sign this message to login with address ${accounts[0]}`,
       };
-      setAddress(payload && payload.username)
-    }
-    catch (err: any) {
-      setAddress('')
+      setAddress(payload && payload.username);
+    } catch (err: any) {
+      setAddress("");
     }
   };
   return (
     <div className="App">
       <header className="App-header">
-        <div onClick={handleConnectMetaMask} className='meta-mask'>
+        <div onClick={handleConnectMetaMask} className="meta-mask">
           <span>Connect to wallet </span>
         </div>
-        {address && (
-          <div>
-            Address: {address}
-          </div>
-        )}
+        {address && <div>Address: {address}</div>}
       </header>
     </div>
   );
-}
+};
 export default App;
